8e0c1fcefdfe779dab93e84a85d9f9466c6da04b
Jerome-Horton <riqui.howard@gmail.com>
2022-01-02 20:22:41 -0600

    created Success Page container and .css file

---
+++ src/components/SuccessPage/SuccessPage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+// import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+// import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+
+function SuccessPage (props) {
+
+    return (
+        <div>
+            <Container className='white-container' maxWidth="xl">
+                <h1 className='success'>Success!</h1>
+                <h1 className='complete'>Your Money Transfer Is <br /> Complete </h1>
+                <h1 className='reference'>Reference Number: 3570941192 </h1>
+
+                <Button className='return-btn' variant="contained">
+                    Return Home   
+                </Button>
+
+            </Container>
+        </div>
+    )
+}
+
+export default SuccessPage;
