07bca2ac70a37e6f96243dbc6e41522385e7c8cf
Jerome-Horton <riqui.howard@gmail.com>
2022-01-02 22:45:18 -0600

    Created Header/nav bar component

--- src/components/SuccessPage/SuccessPage.js
+++ src/components/SuccessPage/SuccessPage.js
@@ -1,13 +1,21 @@
 import React from 'react';
 // import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-// import Grid from '@mui/material/Grid';
+import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
+
+// Nav Bar / Header Imports
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import { IconFlagUS } from 'material-ui-flags';
 
 function SuccessPage (props) {
 
     return (
+
         <div>
+        
             <Container className='white-container' maxWidth="xl">
                 <h1 className='success'>Success!</h1>
                 <h1 className='complete'>Your Money Transfer Is <br /> Complete </h1>
