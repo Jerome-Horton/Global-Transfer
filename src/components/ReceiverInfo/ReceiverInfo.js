6dc966e4b537e4caaf7f38159412fa75aadff89e
Jerome-Horton <riqui.howard@gmail.com>
2022-01-11 03:43:43 -0600

    function reveiverPage to update database

--- src/components/ReceiverInfo/ReceiverInfo.js
+++ src/components/ReceiverInfo/ReceiverInfo.js
@@ -1,63 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
+import './ReceiverInfo.css';
+
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
-import './ReceiverInfo.css'
-import Header from '../Header/Header';
-
-// import { useNavigate } from 'react-router';
 
 
 function ReceiverPage() {
 
-    function PaymentInfo() {
-        // navigate('/paymentInfo')
-    }
+    let navigate = useHistory();
+    const dispatch = useDispatch();
+
+    const receiverInfo = useSelector(state => state.userInfo.receiverInfo)
+
+    const [firstName, setFirstName] = useState(receiverInfo.firstName || "")
+    const [middleName, setMiddleName] = useState(receiverInfo.middleName || "")
+    const [lastName, setLastName] = useState(receiverInfo.lastName || "")
+    const [phone, setPhone] = useState(receiverInfo.phone || "")
+    const [address, setAddress] = useState(receiverInfo.address || "")
+
+
 
     return (
 
         <div >
 
-            <Header />
-
-            <Container className='white-container-receiver' maxWidth="xl">
-                <p className='receiver-info-text'>Receiver Information</p>
-                <Grid className='grid-receiver' container spacing={2}>
+            <Container className='white-container-sender' maxWidth="xl">
+                <p className='sender-info-text'>Receiver Information</p>
+                <Grid className='grid-sender' container spacing={2}>
                     <Grid item xs={12} md={6} >
-                        <input className='receiver-input-info' placeholder='Legal First Name' type="text" />
+                        <input className='sender-input-info'
+                            value={firstName}
+                            onChange={(e) => { setFirstName(e.target.value) }}
+
+                            placeholder='Legal First Name' type="text" />
                     </Grid>
                     <Grid item xs={12} md={6}>
-                        <input className='receiver-input-info' placeholder='Legal Middle Name' type="text" />
+                        <input className='sender-input-info'
+                            value={middleName}
+                            onChange={(e) => { setMiddleName(e.target.value) }}
+
+
+                            placeholder='Legal Middle Name' type="text" />
                     </Grid>
 
                 </Grid>
-                <Grid className='grid-receiver' container spacing={2}>
+                <Grid className='grid-sender' container spacing={2}>
                     <Grid item xs={12} md={6} >
-                        <input className='receiver-input-info' placeholder='Legal Last Name' type="text" />
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <input className='receiver-input-info' placeholder='Second Last Name' type="text" />
+                        <input className='sender-input-info'
+                            value={lastName}
+                            onChange={(e) => { setLastName(e.target.value) }}
+
+
+                            placeholder='Legal Last Name' type="text" />
                     </Grid>
 
                 </Grid>
                 <div className='center-text'>Additional Receiver Information</div>
-                <Grid className='grid-receiver' container spacing={2}>
+                <Grid className='grid-sender' container spacing={2}>
                     <Grid item xs={12} md={6} >
-                        <input className='receiver-input-info' placeholder='Address' type="text" />
+                        <input className='sender-input-info'
+
+                            value={address}
+                            onChange={(e) => { setAddress(e.target.value) }}
+
+
+                            placeholder='Address' type="text" />
                     </Grid>
                     <Grid item xs={12} md={6}>
-                        <input className='receiver-input-info' placeholder='Address' type="text" />
+                        <input className='sender-input-info'
+
+                            value={phone}
+                            onChange={(e) => { setPhone(e.target.value) }}
+
+
+                            placeholder='Phone Number' type="text" />
                     </Grid>
 
                 </Grid>
-                <Grid className='grid-receiver' container spacing={2}>
-                    <Grid item xs={12} md={6} >
-                        <input className='receiver-input-info' placeholder='Phone Number' type="text" />
-                    </Grid>
+                <Grid className='grid-sender' container spacing={2}>
+
 
                     <Grid item xs={12} md={6}>
                         <div className='receiver-btn-container'>
-                            <Button className='receiver-info-btn' onClick={() => PaymentInfo()} variant="contained">Next</Button>
+                            <Button className='sender-info-btn' onClick={() => PaymentInfo()} variant="contained">Next</Button>
                         </div>
                     </Grid>
 
