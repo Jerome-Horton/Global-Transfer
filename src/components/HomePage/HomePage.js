ddbcca5f1201fab3bd3676d438fde79f01014176
Jerome-Horton <riqui.howard@gmail.com>
2022-01-11 00:08:45 -0600

    imported muis assets and styled dropdown

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -15,16 +15,29 @@ import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TextField from '@mui/material/TextField';
 import TabPanel from '@mui/lab/TabPanel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import USFlag from "../../assets/images/flag.png"
+import { makeStyles } from '@mui/styles';
 
-
-
-
-
+const useStyles = makeStyles({
+    dropdownStyle:
+    {
+        border: "1px solid black",
+        borderRadius: "5%",
+        backgroundColor: 'lightgrey',
+    },
+    inputStyle: {
+        color: "red"
+    }
+});
 
 
 function homePage() {
 
     const [value, setValue] = React.useState('1');
+
+    const classes = useStyles()
 
 
     let navigate = useHistory();
@@ -69,111 +82,111 @@ function homePage() {
     }
 
 
-function trackYourMoney() {
+    function trackYourMoney() {
 
-    const data = {
-        reference, firstName: fullName.split(" ")[0],
-        lastName: fullName.split(" ")[1]
+        const data = {
+            reference, firstName: fullName.split(" ")[0],
+            lastName: fullName.split(" ")[1]
+        }
+        dispatch({
+            type: 'GET_TRANSFER_STATUS',
+            payload: data
+        })
+        navigate.push('/transfer-status')
+
     }
-    dispatch({
-        type: 'GET_TRANSFER_STATUS',
-        payload: data
-    })
-    navigate.push('/transfer-status')
 
-}
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
-const handleChange = (event, newValue) => {
-    setValue(newValue);
-};
+    function sendNow() {
+        navigate.push('/')
+    }
 
-function sendNow() {
-    navigate.push('/')
-}
+    const countryName = () => {
+        const id = currencyList.findIndex(currency => currency.id === selectCountry)
+        return currencyList[id].full_name
+    }
 
-const countryName = () => {
-    const id = currencyList.findIndex(currency => currency.id === selectCountry)
-    return currencyList[id].full_name
-}
+    return (
+        <div >
 
-return (
-    <div >
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid className='nav-grid-track' container spacing={2}>
 
-        <Box sx={{ flexGrow: 1 }}>
-            <Grid className='nav-grid-track' container spacing={2}>
+                    <Grid item xs={3} md={3} >
+                        <h4>Money Transfer At Your Finger Tips</h4>
+                        <h4>Send Money Now Starting For $0 Fee*</h4>
+                        <a className="send-link" onClick={sendNow} >Send Now</a>
+                    </Grid>
 
-                <Grid item xs={3} md={3} >
-                    <h4>Money Transfer At Your Finger Tips</h4>
-                    <h4>Send Money Now Starting For $0 Fee*</h4>
-                    <a className="send-link" onClick={sendNow} >Send Now</a>
+                    <Grid textAlign="center" item xs={3} md={7}>
+                        <Container className='white-container-track' >
+                            <Box sx={{ width: '100%', Typography: 'body1' }}>
+                                <TabContext value={value}>
+                                    <Box  >
+                                        <TabList onChange={handleChange}  >
+                                            <Tab label="Rates & Fees" value="1" />
+                                            <Tab label="Track & receive" value="2" />
+                                        </TabList>
+                                    </Box>
+
+                                    <TabPanel value="1" className="tabs-each">
+                                        <input className="input-field-1" placeholder='Send Amount'
+                                            value={amount} onChange={(e) => { setAmount(e.target.value) }}
+                                        /><span className='usd-name'>USD <img width="40px" src={USFlag} /></span>
+
+                                        {/* <InputLabel id="demo-simple-select-label">Receiver Country</InputLabel> */}
+                                        <Select
+                                            value={selectCountry}
+                                            displayEmpty
+                                            // MenuProps={{ classes: { paper: classes.dropdownStyle } }}
+                                            renderValue={
+                                                selectCountry !== "" ?
+                                                    () => <div className="input-field render-large-value" >{countryName()}</div>
+                                                    :
+                                                    () => <div className="input-field render-value" >Receiver Country</div>
+                                            }
+
+                                            className="input-field"
+                                            onChange={(e) => { setSelectCountry(e.target.value) }}
+                                        >
+                                            {
+                                                currencyList.map(currency =>
+                                                    <MenuItem key={currency.id} value={currency.id}>{currency.full_name}</MenuItem>
+                                                )
+                                            }
+                                        </Select>
+
+                                        {/* <input className="input-field" placeholder='Receiver Country' /> */}
+
+                                        <Button className='estimate-btn' onClick={() => estimateFees()} variant="contained"
+                                            disabled={!amount && !selectCountry}> Estimate Fees</Button>
+                                    </TabPanel>
+
+                                    <TabPanel value="2" className="tabs-each">
+
+                                        <TextField id="standard-basic" className="input-fields" label="Reference Number"
+                                            onChange={(e) => { setReference(e.target.value) }}
+                                            value={reference} variant="standard" />
+
+                                        <TextField id="standard-basic" className="input-fields" label="Full Name" variant="standard"
+                                            onChange={(e) => { setFullName(e.target.value) }} value={fullName} />
+
+                                        <Button className='track-btn' onClick={trackYourMoney} variant="contained"
+                                            disabled={!reference && !fullName}>Track Your Money</Button>
+                                    </TabPanel>
+                                </TabContext>
+                            </Box>
+                        </Container>
+                    </Grid>
+
                 </Grid>
+            </Box>
 
-                <Grid textAlign="center" item xs={3} md={7}>
-                    <Container className='white-container-track' >
-                        <Box sx={{ width: '100%', Typography: 'body1' }}>
-                            <TabContext value={value}>
-                                <Box  >
-                                    <TabList onChange={handleChange}  >
-                                        <Tab label="Rates & Fees" value="1" />
-                                        <Tab label="Track & receive" value="2" />
-                                    </TabList>
-                                </Box>
-
-                                <TabPanel value="1" className="tabs-each">
-                                    <input className="input-field-1" placeholder='Send Amount'
-                                        value={amount} onChange={(e) => { setAmount(e.target.value) }}
-                                    /><span className='usd-name'>USD <img width="40px" src={USFlag} /></span>
-
-                                    {/* <InputLabel id="demo-simple-select-label">Receiver Country</InputLabel> */}
-                                    <Select
-                                        value={selectCountry}
-                                        displayEmpty
-                                        // MenuProps={{ classes: { paper: classes.dropdownStyle } }}
-                                        renderValue={
-                                            selectCountry !== "" ?
-                                                () => <div className="input-field render-large-value" >{countryName()}</div>
-                                                :
-                                                () => <div className="input-field render-value" >Receiver Country</div>
-                                        }
-
-                                        className="input-field"
-                                        onChange={(e) => { setSelectCountry(e.target.value) }}
-                                    >
-                                        {
-                                            currencyList.map(currency =>
-                                                <MenuItem key={currency.id} value={currency.id}>{currency.full_name}</MenuItem>
-                                            )
-                                        }
-                                    </Select>
-
-                                    {/* <input className="input-field" placeholder='Receiver Country' /> */}
-
-                                    <Button className='estimate-btn' onClick={() => estimateFees()} variant="contained"
-                                        disabled={!amount && !selectCountry}> Estimate Fees</Button>
-                                </TabPanel>
-
-                                <TabPanel value="2" className="tabs-each">
-
-                                    <TextField id="standard-basic" className="input-fields" label="Reference Number"
-                                        onChange={(e) => { setReference(e.target.value) }}
-                                        value={reference} variant="standard" />
-
-                                    <TextField id="standard-basic" className="input-fields" label="Full Name" variant="standard"
-                                        onChange={(e) => { setFullName(e.target.value) }} value={fullName} />
-
-                                    <Button className='track-btn' onClick={trackYourMoney} variant="contained"
-                                        disabled={!reference && !fullName}>Track Your Money</Button>
-                                </TabPanel>
-                            </TabContext>
-                        </Box>
-                    </Container>
-                </Grid>
-
-            </Grid>
-        </Box>
-
-    </div>
-);
+        </div>
+    );
 }
 
 export default homePage;
