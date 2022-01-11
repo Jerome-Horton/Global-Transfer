1dac57b2aa840938858826602838184d96da06fe
Jerome-Horton <riqui.howard@gmail.com>
2022-01-11 02:59:02 -0600

    linked status page with api/route to query transactions tatus

--- src/components/StatusPage/StatusPage.js
+++ src/components/StatusPage/StatusPage.js
@@ -1,6 +1,6 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router';
-import {useSelector,useDispatch} from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
@@ -8,19 +8,19 @@ import Container from '@mui/material/Container';
 
 import './StatusPage.css'
 
-function statusPage () {
+function statusPage() {
 
-const dispatch = useDispatch()
-let navigate = useHistory();
+    const dispatch = useDispatch()
+    let navigate = useHistory();
 
-const trackingInfo = useSelector(state => state.tracking)
+    const trackingInfo = useSelector(state => state.tracking)
 
-useEffect(() => {
-      
-    return () => {
-      dispatch({type: 'CLEAR_TRACKING_INFO'})
-    }
-  }, [])
+    useEffect(() => {
+
+        return () => {
+            dispatch({ type: 'CLEAR_TRACKING_INFO' })
+        }
+    }, [])
 
 
 
@@ -32,43 +32,56 @@ useEffect(() => {
 
             <Container className='white-container-transfer' maxWidth="xl">
                 <p className='transfer-status'>Transfer Status</p>
-                <Grid justifyContent="around" className='transfer-status-grid' container spacing={2}>
-                    <Grid item xs={12} sm={6} >
-                        <h4 className='transfer-header'>Receiver Country:</h4>
-                        <p className='transfer-text'>Liberia</p>
-                    </Grid>
 
-                    <Grid item xs={12} sm={6} >
-                        <h4 className='transfer-header'>Amount:</h4>
-                        <p className='transfer-text'>200.00 USD</p>
-                    </Grid>
+                {trackingInfo.legal_first_name ?
+                    <>
+                        <Grid justifyContent="around" className='transfer-status-grid' container spacing={2}>
+                            <Grid item xs={12} sm={6} >
+                                <h4 className='transfer-header'>Receiver Country:</h4>
+                                <p className='transfer-text'>{trackingInfo.full_name}</p>
+                            </Grid>
+
+                            <Grid item xs={12} sm={6} >
+                                <h4 className='transfer-header'>Sent Amount:</h4>
+                                <p className='transfer-text'>{trackingInfo.sent_amount} USD</p>
+                            </Grid>
+                            <Grid item xs={12} sm={6} >
+                                <h4 className='transfer-header'>Receiving Amount:</h4>
+                                <p className='transfer-text'>{trackingInfo.receiving_amount} {trackingInfo.currency_name}</p>
+                            </Grid>
+
+                        </Grid>
+                        <Grid justifyContent="around" className='transfer-status-grid' container spacing={2}>
+                            <Grid item xs={12} sm={6} >
+                                <h4 className='transfer-header'>Receiver:</h4>
+                                <p className='transfer-text'>{trackingInfo.legal_first_name + " " +
+                                    trackingInfo.legal_last_name
+                                }</p>
+                            </Grid>
+
+                            <Grid item xs={12} sm={6} >
+                                <h4 className='transfer-header'>Receive Method:</h4>
+                                <p className='transfer-text'>{trackingInfo.receiving_method}</p>
+                            </Grid>
+
+                        </Grid>
+                        <Grid justifyContent="around" className='transfer-status-grid' container spacing={2}>
+                            <Grid item xs={12} sm={6} >
+                                <h4 className='transfer-header'>Reference Number:</h4>
+                                <p className='transfer-text'>{trackingInfo.reference_number}</p>
+                            </Grid>
+                        </Grid>
+                    </> :
+                    <p className='transfer-status'>Not Found</p>
+                }
+
+                <Grid item xs={12} sm={6} >
+
+                    <Button className='home-btn' onClick={() => homeBtn()} variant="contained">Home</Button>
 
                 </Grid>
-                <Grid justifyContent="around" className='transfer-status-grid' container spacing={2}>
-                    <Grid item xs={12} sm={6} >
-                        <h4 className='transfer-header'>Receiver:</h4>
-                        <p className='transfer-text'>Jerome Horton</p>
-                    </Grid>
 
-                    <Grid item xs={12} sm={6} >
-                        <h4 className='transfer-header'>Receive Method:</h4>
-                        <p className='transfer-text'>Cash Pickup</p>
-                    </Grid>
-                </Grid>
-
-                <Grid justifyContent="around" className='transfer-status-grid' container spacing={2}>
-                    <Grid item xs={12} sm={6} >
-                        <h4 className='transfer-header'>Reference Number:</h4>
-                        <p className='transfer-text'>3570941192</p>
-                    </Grid>
-
-                    <Grid item xs={12} sm={6} >
-
-                        <Button className='home-btn' onClick={() => homeBtn()} variant="contained">Home</Button>
-
-                    </Grid>
-
-                </Grid>
+                {/* </Grid> */}
 
             </Container>
         </div>
