ebacdd22e951e452e4a3a44eb5d51e753d845b01
Jerome-Horton <riqui.howard@gmail.com>
2022-01-11 03:21:17 -0600

    imported sweetalerts and function Edit button functionality

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,16 +1,46 @@
 import React from 'react';
+import { useHistory } from 'react-router';
+import { useSelector, useDispatch } from 'react-redux';
+import './Summary.css';
+
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
-import Header from '../Header/Header';
-import './Summary.css';
-
-import { useNavigate } from 'react-router';
+import swal from 'sweetalert2';
+import Axios from 'axios';
 
 
 function Summary() {
 
-    let navigate = useNavigate();
+    const dispatch = useDispatch()
+    let navigate = useHistory();
+
+    const transaction = useSelector(state => state.transaction)
+    const receiverInfo = useSelector(state => state.userInfo.receiverInfo)
+
+    const transactionConfirmation = () => {
+
+        const data = {
+            firstName: receiverInfo.firstName,
+            middleName: receiverInfo.middleName,
+            lastName: receiverInfo.lastName,
+            address: receiverInfo.address,
+            phone: receiverInfo.phone,
+            amount: transaction.amount,
+            receivingAmount: transaction.convertedValue,
+            currencyID: transaction.selectCountry
+        }
+
+        axios.post('/api/transaction', data).then(response => {
+            dispatch({
+                type: "SET_REFERENCE_NUMBER",
+                payload: response.data[0].reference_number
+            })
+            navigate.push("/success")
+        }
+        ).catch(err => console.log(err))
+
+    }
 
     function nextBtn() {
         navigate('/success')
@@ -19,21 +49,23 @@ function Summary() {
     return (
 
         <div >
-            <Header />
+
             <Container className='white-container-summary' maxWidth="xl">
                 <p className='summary-text'>Summary</p>
                 <h6 className='almost-done'>Almost Done! Review the information below and complete your transfer.</h6>
-                <h5 className='rates-fees'>Rate & Fees <span className='edit-btn'><button>Edit</button></span> </h5>
-                <p className='conversion-rate'>1.0000 USD = 1.0000 USD 1</p>
+                <h5 className='rates-fees'>Rate & Fees <span className='edit-btn'>
+                    <button onClick={() => navigate.push('/')}>Edit</button></span> </h5>
+                <p className='conversion-rate'>{`${transaction.amount} USD = 
+          ${transaction.convertedValue} ${transaction.currencyName} Exchange Rate: ${transaction.conversionRate} `}</p>
 
                 <Grid className='summary-small-boxes' container spacing={2}>
                     <Grid item xs={3} md={3} >
                         <h4 className='small-box-header'>Receiver Country</h4>
-                        <p className='small-box-text'>Liberia</p>
+                        <p className='small-box-text'>{transaction.countryFullName}</p>
                     </Grid>
                     <Grid item xs={3} md={3}>
                         <h4 className='small-box-header'>Amount</h4>
-                        <p className='small-box-text'>200.00 USD</p>
+                        <p className='small-box-text'>{`${transaction.convertedValue} ${transaction.currencyName}`}</p>
 
                     </Grid>
 
@@ -46,17 +78,19 @@ function Summary() {
 
                 </Grid>
 
-                <h5 className='rates-fees'>Receiver <span className='edit-btn'><button>Edit</button></span> </h5>
+                <h5 className='rates-fees'>Receiver <span className='edit-btn'>
+                    <button onClick={() => navigate.push('/receiver-info')}>Edit</button></span> </h5>
                 <p className='conversion-rate'>Legal Name</p>
-                <p className='conversion-rate'>Jerome Horton</p>
+                <p className='conversion-rate'>{`${receiverInfo.firstName} ${receiverInfo.lastName}`}</p>
 
-                <h5 className='rates-fees'>COST SUMMARY <span className='edit-btn'><button>Edit</button></span> </h5>
+                <h5 className='rates-fees'>COST SUMMARY <span className='edit-btn'>
+                    <button onClick={() => navigate.push('/getStarted')}>Edit</button></span> </h5>
                 <Grid className='summary-small-boxes' container spacing={2}>
                     <Grid item xs={6} md={3} >
                         <h4 className='small-box-header'>Total Cost:</h4>
-                        <p className='small-box-text'>200.00 USD</p><br />
+                        <p className='small-box-text'>{`${transaction.amount} USD`}</p><br />
                         <h4 className='small-box-header'>Total Receive Amount:</h4>
-                        <p className='small-box-text'>200.00 USD</p>
+                        <p className='small-box-text'>{`${transaction.convertedValue} ${transaction.currencyName}`}</p>
                     </Grid>
 
                     <Grid item xs={6} md={4} >
