a2a1663b058b2a8c1a06293d38c4875264452c19
Jerome-Horton <riqui.howard@gmail.com>
2022-01-15 20:43:12 -0600

    Created reducer and saga for transaction history

--- src/redux/sagas/transaction.saga.js
+++ src/redux/sagas/transaction.saga.js
@@ -14,20 +14,23 @@ function* fetchTransaction(action) {
   }
 }
 
-function* fetchTransactionAll(action) {
+function* getTransactionHistory(action) {
   try {
     console.log(action.payload)
-    const response = yield axios.post('/api/transaction/tracking',action.payload)
+    const response = yield axios.post('/api/transaction',action.payload)
 
-    yield put({ type: 'SHOW_TRANSACTION_INFO', payload : response.data[0] })
+    yield put({ 
+      type: 'DISPLAY_TRANSACTION', 
+      payload : response.data[0] })
 
   } catch (error) {
-    console.log('Error with fetch transaction status saga:', error)
+    console.log('Error with GET Transaction History saga:', error)
   }
 }
 
 function* transactionSaga() {
   yield takeLatest('GET_TRANSFER_STATUS', fetchTransaction);
+  yield takeLatest('GET_TRANSACTIONS', getTransactionHistory);
 }
 
 export default transactionSaga;
