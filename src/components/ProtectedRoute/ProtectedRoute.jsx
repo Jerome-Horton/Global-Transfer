a823ca5f09b6b43bc0179efb6cefe2a4354aeb31
Jerome-Horton <riqui.howard@gmail.com>
2022-01-17 12:32:41 -0600

    fixed Delete api - working properly.

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import LoginPage from '../LoginPage/LoginPage';
+import HomePage from '../HomePage/HomePage';
 import {useSelector} from 'react-redux';
 
 // A Custom Wrapper Component -- This will keep our code DRY.
@@ -32,7 +32,8 @@ function ProtectedRoute({ component, children, ...props }) {
         <ProtectedComponent />
         :
         // Otherwise, redirect to the Loginpage
-        <LoginPage />
+        // <LoginPage />
+       <HomePage />
       }
     </Route>
 
